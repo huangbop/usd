b5fd1005220715dc1f3674352311d6e73039ce46
Huang Bo <huangbop@gmail.com>
2015-01-27 19:01:17 +0800

    Funcs

--- include/object.h
+++ include/object.h
@@ -8,18 +8,24 @@ extern "C" {
 struct usd_object {
 	struct usd_object *prev;
 	struct usd_object *next;
-	struct usd_type *type;
 	char *name;
+	struct usd_type *type;
 };
+
+	typedef int (*fn_open)();
+	typedef int (*fn_close)();
+	typedef int (*fn_read)();
+	typedef int (*fn_write)();
+	
 
 struct usd_type {
 	struct usd_object object;
 	char *tp_name;
 
-	/* func_open *tp_open; */
-	/* func_close *tp_close; */
-	/* func_read *tp_read; */
-	/* func_write *tp_write; */
+	fn_open *tp_open;
+	fn_close *tp_close;
+	fn_read *tp_read;
+	fn_write *tp_write;
 	
 };
 	
