7a42e38c25c601f320745f673ed90f7233270ce0
Huang Bo <huangbop@gmail.com>
2015-02-02 22:47:03 +0800

    Add plot view

--- views/tabs/gpio.py
+++ views/tabs/gpio.py
@@ -3,6 +3,7 @@ import sys
     
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+import pyqtgraph as pg
 
 
 class GpioForm(QWidget):
@@ -13,8 +14,18 @@ class GpioForm(QWidget):
         vlayout = QVBoxLayout(self)
         vlayout.setContentsMargins(0, 0, 0, 0)
         vlayout.addWidget(self.bg)
-        self.bg.setStyleSheet("background-color: blue")
+        self.bg.setStyleSheet("""
+        background-color: white;
+        """)
 
+        self.plotview = pg.PlotWidget(self.bg)
+        self.plotview.setStyleSheet("""
+        border-radius: 6px;
+        """)
+        
+        bg_hlayout = QHBoxLayout(self.bg)
+        bg_hlayout.setContentsMargins(15, 10, 150, 10)
+        bg_hlayout.addWidget(self.plotview)
 
 
 
