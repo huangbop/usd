59c543254e455e781c65430ee8e181b4d15041af
Huang Bo <huangbop@gmail.com>
2015-03-29 04:54:51 +0800

    Uart print

--- usd/views/tabs/uart.py
+++ usd/views/tabs/uart.py
@@ -5,6 +5,9 @@ import string
     
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from serial.serialwin32 import Win32Serial 
+from serial.tools.list_ports import comports
+import re
 
 from usd.views.ui.Ui_uart import Ui_uart
 from usd.views.setting import tabs_style
@@ -14,13 +17,15 @@ class PrintThread(QThread):
 
     yield_text = pyqtSignal(str)
 
-    def __init__(self):
+    def __init__(self, serial):
         QThread.__init__(self)
+        self.serial = serial
 
     def run(self):
+        if not self.serial.isOpen():
+            return
         while True:
-            self.yield_text.emit(''.join(random.sample(string.printable, 90)))
-            self.msleep(100)
+            self.yield_text.emit(self.serial.read().decode())
 
 
 class UartForm(QWidget, Ui_uart):
@@ -30,28 +35,47 @@ class UartForm(QWidget, Ui_uart):
 
         self.setStyleSheet(tabs_style)
 
-        self.log.setReadOnly(False)
+        self.log.setReadOnly(True)
         self.log.setMaximumBlockCount(5000)
 
         # Connect signals
         self.btn_startstop.clicked.connect(self.startstop_clicked)
 
+        # Enum serial devices
+        self.serial = Win32Serial(baudrate=115200)
+        self.serial_devices = sorted(comports())
+        print(self.serial_devices)
+
         # Print thread
-        self.print_thread = PrintThread()
+        self.print_thread = PrintThread(self.serial)
         self.print_thread.yield_text.connect(self.do_print)
 
     def startstop_clicked(self):
         if self.btn_startstop.text() == "Start":
-            if not self.print_thread.isRunning():
-                self.print_thread.start()
-            self.btn_startstop.setText("Stop")
+            matched = False
+            for port, desc, hwid in sorted(comports()):
+                if re.match(r'PORTS\\VID_04B4&PID_0005.*&01', hwid):
+                    matched = True
+                    break
+            if not matched:
+                QMessageBox.information(None, "Warning",
+                                        "No uart-diagnose board found.")
+            else:
+                self.serial.setPort(port)
+                if not self.serial.isOpen():
+                    self.serial.open()
+
+                if not self.print_thread.isRunning():
+                    self.print_thread.start()
+                    self.btn_startstop.setText("Stop")
+
         else:
             if self.print_thread.isRunning():
                 self.print_thread.terminate()
+            self.serial.close()
             self.btn_startstop.setText("Start")
 
     def do_print(self, str):
-        self.log.appendPlainText(str)
+        self.log.insertPlainText(str)
         self.log.verticalScrollBar().setSliderPosition(
             self.log.verticalScrollBar().maximum())
-        
